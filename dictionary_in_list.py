6e3ee8cebf42e855cd6b4be4265474540b60e784
pdr0k <gpedrocaique@gmail.com>
2023-09-25 18:12:20 -0300

    add dictionary_in_list - exercise 2

---
+++ dictionary_in_list.py
@@ -0,0 +1,19 @@
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+
+def add_new_country(country_visited, times, cities_visited):
+    travel_log.append({"country":country_visited, "visits": times,"cities":cities_visited})
+
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+
+print(travel_log)
